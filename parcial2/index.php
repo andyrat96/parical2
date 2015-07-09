667ba38cd9368fee0a15d7891930162965d36b0a
Andy Ayala <andyrat96@hotmail.com>
2015-07-09 16:04:08 -0500

    Parcial2
    Entrega de la actividad

---
+++ parcial2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Evaluación del segundo parcial.</title>
+    
+</head>
+<body>
+   <form action="funciones.php" method="post">
+        <select id="estructura" name="estructura">
+            <option value="elfor">FOR</option>
+            <option value="eldowhile">DO-WHILE</option>
+            <option value="elwhile">WHILE</option>
+        </select>
+    
+    <input type="submit" value="Enviar">
+    </form>
+</body>
+
+
+</html>
+
+
+
+
+
+
