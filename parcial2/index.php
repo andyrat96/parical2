dce6552340ea1d7867575f2f8ecc0e2990a820cf
andyrat96 <gol_chivas_96@hotmail.com>
2015-07-09 20:17:17 -0500

    Update index.php

--- parcial2/index.php
+++ parcial2/index.php
@@ -3,25 +3,16 @@
 <head>
     <meta charset="utf-8">
     <title>Evaluación del segundo parcial.</title>
-    
 </head>
 <body>
+<h1>Tablas de Multiplicar</h1>
    <form action="funciones.php" method="post">
-        <select id="estructura" name="estructura">
-            <option value="elfor">FOR</option>
-            <option value="eldowhile">DO-WHILE</option>
-            <option value="elwhile">WHILE</option>
+        <select id="ciclo" name="ciclo">
+            <option value="1">WHILE</option>
+            <option value="2">DO-WHILE</option>
+            <option value="3">FOR</option>
         </select>
-    
     <input type="submit" value="Enviar">
     </form>
 </body>
-
-
 </html>
-
-
-
-
-
-
